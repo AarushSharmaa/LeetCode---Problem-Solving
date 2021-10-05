fd536890ff474adf65e75f7699a88464f493b31d
Aarush Sharma <aarush.sharma6@gmail.com>
2021-10-05 16:53:32 +0530

    Time: 5570 ms (5.00%), Space: 13.8 MB (99.98%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -2,29 +2,39 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         
         #Brute Force Thinking 
-        #Clears 986/987 test cases and then throws TLE
+        #All test cases passed
+        #O(N^2) Time 
         
         
         n = len(s)
         answer = 0 
         for i in range(0, n):
-            #substring = ""
+            substring = ""
             d = {}
             for j in range(i, n):
             
-        #way 1 to check if a substring has distinct characters or n
+        #WAY 1 -- to check if a substring has distinct characters or not
+        #This clears 986/987 test cases and then throws TLE at the last test case
         
-                #substring += s[j]
-                #if len(set(substring)) == len(substring):
-                    #answer = max(answer, j - i + 1)
-            
-        #way 2 to check if a substring has distinct characters or not
-        
-                if s[j] in d:
-                    break
-                else:
-                    d[s[j]] = 1
+                substring += s[j]
+                if len(set(substring)) == len(substring):
                     answer = max(answer, j - i + 1)
+                else:
+                    break
+        
+        #(j - i + 1) simply represents the length of substring being considered 
+        #which starts at ith index and ends at jth index
+            
+        #WAY 2 -- to check if a substring has distinct characters or not
+        #This is an accepted solution
+        #Remember, looking somethin up in Dictionary is O(1)
+                #if (s[j] in d) and (d[s[j]]) > 0 :
+                #    break
+                #else:
+                #    d[s[j]] = 1
+                #    answer = max(answer, j - i + 1)
         return answer
+        
+        
         
         
