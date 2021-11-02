00791aa7055ae84cb6903d8cf2a15ed8c4f3de1b
Aarush Sharma <aarush.sharma6@gmail.com>
2021-11-02 11:34:05 +0530

    Time: 800 ms (94.27%), Space: 76.5 MB (5.44%) - LeetHub

--- first-missing-positive/first-missing-positive.py
+++ first-missing-positive/first-missing-positive.py
@@ -1,7 +1,18 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         
-
+        
+        #BRUTE FORCE SOLUTION
+        """
+        n = len(nums)
+        
+        for i in range(1, n+1):
+            if i not in nums:
+                return i
+        return i + 1
+        """
+        
+        #OPTIMAL SOLUTION
         
         n = len(nums)
         
@@ -12,11 +23,14 @@ class Solution:
         
         #populating the dictionary with elements as the keys
         #as lookups in dictionary will take O(1) Time
+        #values corresponding to elements don't really matter
         for i in nums:
             d[i] = 0 
         
+        
         for i in range(1, n+1):
-            if i not in d:
+            if i not in d: #this will not take O(N) Time but will take O(1) Time only
+                #as lookups in Hashtables are much faster than lookups in an array
                 return i 
-        else:
-            return i + 1
+        return i + 1
+        
