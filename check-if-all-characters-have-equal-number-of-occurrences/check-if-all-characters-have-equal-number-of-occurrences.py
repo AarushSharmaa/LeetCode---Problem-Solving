806c5f1a71198b6d6334a8b9cb6d8291acf70c73
Aarush Sharma <aarush.sharma6@gmail.com>
2021-11-04 13:48:25 +0530

    Time: 84 ms (5.08%), Space: 14.3 MB (32.32%) - LeetHub

--- check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.py
+++ check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.py
@@ -3,6 +3,7 @@ class Solution:
         
         #O(N) TIME AND O(N) SPACE
         
+        """
         dictionary = {} #this will hold elements corresponding to its frequency in the hashtable
         
         for i in s:
@@ -22,4 +23,27 @@ class Solution:
             return True
         else:
             return False
+        """
+        
+        #Let us experiment with a more basic thought process
+        #O(N^2) TIME AND O(N) SPACE
+        
+        s1 = list(set(s))
+        n1 = len(s1)
+        n = len(s)
+        output = []
+        
+        for i in range(0, n1):
+            count = 0 
+            for j in range(0, n):
+                if s1[i] == s[j]:
+                    count += 1 
+            output.append(count)
+            
+        if len(set(output)) == 1:
+            return True
+        else:
+            return False
+        
+        
         
