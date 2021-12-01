255f5c9a97cd3f52affae98fb5947417e4dba550
Aarush Sharma <aarush.sharma6@gmail.com>
2021-12-02 01:45:56 +0530

    Time: 72 ms (77.22%), Space: 14.4 MB (40.23%) - LeetHub

--- two-out-of-three/two-out-of-three.py
+++ two-out-of-three/two-out-of-three.py
@@ -1,8 +1,11 @@
 class Solution:
     def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
         
+        #Time - O(N) and Space - O(N)
+        
         #return a distinct array containing all the values 
         #that are present in at least two out of the three arrays.
+        #so, we will return all values that are present in 2 or 3 arrays 
         
         #NOTE - values can be in any order
         
@@ -11,6 +14,7 @@ class Solution:
         d3 = {} #will hold the elements and its frequences of nums3
         
         #populating d1 with elements of nums1
+        #O(N) Time
         for i in nums1:
             if i in d1:
                 d1[i] += 1
@@ -18,6 +22,7 @@ class Solution:
                 d1[i] = 1
         
         #populating d2 with elements of nums2
+        #O(N) Time
         for i in nums2:
             if i in d2:
                 d2[i] += 1
@@ -25,6 +30,7 @@ class Solution:
                 d2[i] = 1
         
         #populating d3 with elements of nums3
+        #O(N) Time
         for i in nums3:
             if i in d3:
                 d3[i] += 1
@@ -39,19 +45,22 @@ class Solution:
         #dictionary or not. If it is, we add it to our output.
         
         
+        #O(N) Time
         for element in d1:
             if element in d2 or element in d3:
                 output.append(element)
-                
+        
+        #O(N) Time
         for element in d2:
             if element in d1 or element in d3:
                 output.append(element)
-                
+        
+        #O(N) Time
         for element in d3:
             if element in d2 or element in d1:
                 output.append(element)
                 
-        answer = set(output)
+        answer = set(output) #set will remove all the duplicates and will let only unique values remain 
         return list(answer)
         
         
