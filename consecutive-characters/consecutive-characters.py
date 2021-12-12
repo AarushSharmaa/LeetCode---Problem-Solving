883ce4e20cd8787d0b97ff25394a9652f462e6fe
Aarush Sharma <aarush.sharma6@gmail.com>
2021-12-12 08:09:15 +0530

    Time: 48 ms (44.24%), Space: 14.3 MB (12.42%) - LeetHub

--- consecutive-characters/consecutive-characters.py
+++ consecutive-characters/consecutive-characters.py
@@ -1,8 +1,14 @@
 class Solution:
     def maxPower(self, s: str) -> int:
         
+        #Requirement : To find out the maximum length of a substring containining only unique character
+        
+        #Logic 1 : We will iterate over the string and if the current element == next element, we will increase our count. 
+        #If its not, we will re-initialise the count to 1. To find out the maximum count among all the counts that came
+        #we will use an answer variable. 
         
         #Complexity Analysis - O(N) Time and O(1) Space
+        
         
         n = len(s)
         
@@ -20,4 +26,5 @@ class Solution:
             answer = max(answer, count)
             
         return answer
+        
         
