b0a0206607f9ba0456df4dec9f6dc01d5bbb1bf5
Aarush Sharma <aarush.sharma6@gmail.com>
2021-12-18 13:37:28 +0530

    Time: 984 ms (5.05%), Space: 17.2 MB (83.03%) - LeetHub

--- linked-list-cycle-ii/linked-list-cycle-ii.py
+++ linked-list-cycle-ii/linked-list-cycle-ii.py
@@ -7,7 +7,12 @@
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
+        #Approach 1 : Hashtable Approach - O(N) Time and O(N) Space
+        #here, we are using a hashtable to store nodes corresponding to any random value (here, we have used  0 as value and nodes as keys)
+        #and traversing over the linkedlist. if we find any node to be repeated again, we can say it is the first node
+        #of the loop.
         
+        """
         d = {}
         current = head
         while current:
@@ -17,3 +22,39 @@ class Solution:
                 d[current] = 0 
                 current = current.next
         return None
+        """
+        
+        #Approach 2 :  Two Pointers Approach : Complexity Analysis : O(N^2) Time and O(1) Space
+        #first, we will check if there is a cycle or not
+        #if there is not a cycle, we will return Null
+        #if there is a cycle, we will play around with pointers
+        
+        slow = head
+        fast = head
+        iscycle = False #this means that there is no cycle present
+        #we will use this iscycle variable to check if we have a cycle or not
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast: #it means we have a cycle
+                iscycle = True
+                break 
+                
+        if iscycle == False: #it means if there is no cycle
+            return None
+        
+        else: #it means we have a cycle and at some point, slow == fast
+            #now, we will continously revolve this slow variable inside the loop
+            #and at the same time, initialise a variable to head node
+            p = head
+            while 1: #while the statement is True 
+                temp = slow #or fast
+                #we will now continously revolve in the loop till we get temp == p
+                while temp != p: 
+                    temp = temp.next
+                    if temp == slow:
+                        break
+                if temp == p:
+                    return temp #or p 
+                p = p.next
+        
