f72cadb5986c3e6eb3766b639e2ebe71d14ffeec
Aarush Sharma <aarush.sharma6@gmail.com>
2021-12-31 16:43:06 +0530

    Time: 104 ms (6.54%), Space: 17.8 MB (23.73%) - LeetHub

--- linked-list-cycle/linked-list-cycle.py
+++ linked-list-cycle/linked-list-cycle.py
@@ -7,10 +7,12 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         
+        #O(N) Time and O(1) Space
         
+        """
         from collections import OrderedDict
-        #d = OrderedDict()
-        d = {}
+        d = OrderedDict()
+        #d = {}
         current = head
         while current:
             if current in d:
@@ -19,4 +21,27 @@ class Solution:
                 d[current] = 0
                 current = current.next
         return False
+        """
         
+        #O(N) Time and O(1) Space
+        
+        """
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+        """
+        
+        d = {}
+        temp = head
+        while temp:
+            if temp in d:
+                return True
+            else:
+                d[temp] = 0 
+                temp = temp.next
+        return False
