5825ff995d44035f374cfb359fe33d26641a2b33
Aarush Sharma <aarush.sharma6@gmail.com>
2022-01-10 16:49:15 +0530

    Time: 75 ms (10.51%), Space: 14.8 MB (35.08%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -3,7 +3,9 @@ class Solution:
         
         
         #BRUTE FORCE SOLUTION - Iterate over the entire matrix and see if the target exists 
+        #O(M*N) Time and O(1) Space
         
+        """
         row = len(matrix)
         column = len(matrix[0])
         
@@ -11,3 +13,19 @@ class Solution:
             for c in range(0, column):
                 if matrix[r][c] == target:
                     return True 
+        """
+        
+        #Obviously we need to think in terms of Binary Search, as we know that the array is already sorted 
+        
+        for array in matrix:
+            left = 0 
+            right = len(array) - 1
+            while left <= right:
+                mid = (left + right) // 2
+                if target == array[mid]: 
+                    return True
+                elif target > array[mid]:
+                    left += 1 
+                else:
+                    right -= 1
+            
