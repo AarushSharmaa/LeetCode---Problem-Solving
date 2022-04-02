a6e83a9d5bb9dfc9251d43b585121568ba954d81
Aarush Sharma <aarush.sharma6@gmail.com>
2022-04-02 11:55:37 +0530

    Time: 212 ms (82.38%), Space: 18.7 MB (83.80%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,8 +1,10 @@
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
         
-        #O(N^2*log(N^2)) Time and O(1) Space
         
+        #APPROACH 1: O(N^2*log(N^2)) Time and O(N) Space
+        
+        """
         array = []
         for arrays in matrix:
             for element in arrays:
@@ -10,3 +12,34 @@ class Solution:
         
         array.sort()
         return array[k-1]
+        """
+        
+        #APPROACH 2: Let us try to optimise this by using some deductions 
+        #BINARY SEARCH ON ANSWER
+        
+        
+        
+        
+        #APPROACH 3: Heaps Approach 
+        import heapq
+    
+        heap = []
+        
+        for i in range(0, len(matrix)):
+            heapq.heappush(heap, (matrix[i][0], i, 0))
+            count = 0
+            
+        while (heap and count < k):
+            answer, i1, j1 = heapq.heappop(heap)
+            if (j1 + 1) < len(matrix):
+                heapq.heappush(heap, (matrix[i1][j1 + 1], i1, j1 + 1))
+            count += 1
+                
+        return answer
+
+
+        
+        
+        
+        
+    
